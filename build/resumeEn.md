073cceb0679dd72c5edeaa9828efda1a4bfa41ef
uncleF <uncle.funkay@gmail.com>
2015-01-22 10:07:01 +0300

    Initial Commit

---
+++ build/resumeEn.md
@@ -0,0 +1,129 @@
+# Ilya Biryukov
+
+Front End Developer (last revisited on 01/15/2015)
+
+E-mail [uncle.funkay@gmail.com](mailto:uncle.funkay@gmail.com?subject=Regarding Resume)
+
+Phone [+7 918 211-37-92](tel:+79182113792)
+
+Skype [buzz_Killingt0n](skype:buzz_Killingt0n?chat)
+
+# Profile
+
+Advanced front end web developer with 8 years of experience in developing and implementing websites and applications (modern frontend stack ­— HTML5, CSS3, JavaScript). Experience in prototyping and developing user interfaces (sites, apps, e-mails, IPTV, interactive expo booths). Limited expirience in prototyping and developing native apps for the iOS and Android. Limited experience in project management.
+
+*   Rapid learner, able to complete projects fast and efficient.
+*   Deep understanding of modern front end development process and its challenges.
+*   Write clean and comprehensive code.
+*   Good problem solving and collaborative skills.
+*   Staying on the bleeding edge of front end development.
+*   Obsessed with efficient development workflow, personal productivity and constant growth as front end developer.
+*   Advanced English.
+
+## Areas of Expertise
+
+*   Responsive Web Design
+*   Mobile Web Development
+*   Single Page Web App Development
+*   E-mail Development
+*   IPTV UI Development
+*   Landing Page Development
+*   UI/UX in Modern Websites
+*   Usability
+*   Cross-browser Compatibility
+*   Front End Performance Optimization
+*   Critical Rendering Path Optimization
+*   Progressive Enhancement / Graceful Degradation
+*   Accessibility
+*   Standards Compliance
+*   Web Content Management
+*   Project Management
+
+## Skills
+
+*   HTML5, CSS3 (Sass), JavaScript (JQuery, Backbone.js, three.JS, etc.), SVG (Snap.svg, Raphael)
+*   Front end performance and accessibility audit (npm, RubyGems, standalone and web apps)
+*   Node.js (npm, Grunt, Bower), XML
+*   Unity
+*   PHP, SQL
+*   Java, Objective-C
+
+## Tools
+
+*   My own open [development tool](https://github.com/uncleF/TemplateX) (mini-framework) for rapid project workflow setup
+*   Sublime Text, Eclipse, Xcode, Android Studio
+*   Emmet, Grunt, Bower, Gulp, Apache Ant
+*   Adobe Photoshop, Pixelmator, Adobe Illustrator, Adobe Flash, Sketca SVG Editor, Inkscape
+*   Basecamp, InVision, Megaplan, Toggl Desktop
+*   Git, [GitHub](https://github.com/), [BitBucket](https://bitbucket.org/)
+*   Wordpress, Drupal
+
+# Past Employment
+
+## Freelance – March 2014 - Currently
+
+### Front End Developer & Consultant
+
+*   Developed multiple websites and applications.
+*   Built a Grunt-based tool to assemble templates for a proprietary landing page generator.
+*   Consulted multiple clients on front end performance and accessibility.
+
+## [Digital-agency Spider Group](http://spider.ru/en/) – February 2008 - March 2014
+
+### Senior Front End Developer – January 2012 - March 2014
+
+*   Developed multiple websites and applications. Including [hight traffic e-commerce site](http://topliga.ru/).
+*   Developed user interface for the [e-commerce iOS app](https://itunes.apple.com/us/app/fingo.-furniture.-try-before/id567070760?mt=8) from scratch.
+*   Prototyped and built interfaces for the interactive expo booths.
+*   Was responsible for setting up development environment and workflow.
+*   Built a few tools for automating development process.
+*   Consulted co-workers and clients on all matters concerning front end development.
+*   Gained some experience in project management and native development for the iOS and Android.
+
+### Front End Developer – February 2008 - January 2012
+
+*   Developed multiple websites using few CMS developed in-house and out of the box (PHP, Python).
+*   Worked on [high traffic regional information portal](http://www.kuban.ru/).
+*   Participated in development of interfaces for the interactive expo booths.
+*   Created numerous marketing e-mail templates.
+
+## [Kuban Internet Company](http://kubic.ru/) – December 2006 - August 2007
+
+### Front End Developer
+
+*   First professional steps as a fully employed front end developer.
+*   Developed multiple websites using an in-house CMS (Perl-based).
+*   Briefly worked on two hight traffic projects — [regional news portal](http://www.yuga.ru/) and [social networking site](http://www.diary.ru/).
+
+## Freelance – Before December 2006
+
+### Front End Developer
+
+*   String of awful sites for the friends.
+*   First attempts to earn some money.
+*   ...a lot of mistakes were made.
+
+# Education
+
+5 years computer science degree from the [Kuban State University](http://www.kubsu.ru/en/).
+
+# Examples
+
+*   [http://ruport.ru/](http://ruport.ru/)
+*   [http://зарулем23.рф/](http://зарулем23.рф/)
+*   [http://shans23.ru/](http://shans23.ru/)
+*   [http://buhtagold.ru/](http://buhtagold.ru/)
+*   [http://cifra-group.ru/](http://cifra-group.ru/)
+*   [http://emercit.ru/](http://emercit.ru/)
+*   [http://buhtagold.ru/content/7/](http://buhtagold.ru/content/7/) (Interactive map)
+*   [http://gsyp.ru/](http://gsyp.ru/)
+*   [http://gsyp.ru/complex/building/2/](http://gsyp.ru/complex/building/2/)
+*   [http://dvinuli.ru/](http://dvinuli.ru/)
+*   [http://gold-age.org/](http://gold-age.org/)
+*   [http://topliga.ru/](http://topliga.ru/)
+*   [http://fingo.pro/](http://fingo.pro/)
+*   [http://idisplay.spider.ru/](http://idisplay.spider.ru/)
+*   [http://добрый.рф/упаковка](http://добрый.рф/упаковка)
+*   [http://www.arkhyz-jivitsa.ru/landing/](http://www.arkhyz-jivitsa.ru/landing/)
+*   [http://www.megavita-energy.ru/landing/](http://www.megavita-energy.ru/landing/)
+*   [http://moscow.arkhyz-visma.ru/landing/](http://moscow.arkhyz-visma.ru/landing/)
---
+++ gruntfile.js
@@ -0,0 +1,453 @@
+//Gruntfile for the Resume Project
+
+var TITLE							= "Biryujov Ilya – Front End Developer",	// Title
+		LANGUAGE					= "ru",																		// Language
+		BUILD_DIR					= "build",																// Project Build
+		DEVELOPMENT_DIR		= "dev",																	// Project Development
+		RESOURCES_DIR			= "res",																	// Resources (CSS, JavaScript, Fonts etc.)
+		INDEX_PAGE				= "index.html",														// Index Page (Should Contain CRP CSS Styles)
+		TEMPLATES_DIR			= "templates",														// Templates
+		CSS_TEMPLATE			= "_head.html",														// Template Containing CSS Declarations
+		CSS_DIR						= "css",																	// Production CSS
+		SASS_DIR					= "sass-dev",															// Sass
+		CSS_DEV_DIR				= "css-dev",															// Generated CSS
+		CSS_FILENAME			= "styles",																// Production CSS Filename
+		CSS_PRINT					= "print";																// Print CSS Filename
+
+function fillAnArray(ARRAY, PATH) {
+	var RESULT = [];
+	for (var ELEMENT in ARRAY) {
+		RESULT.push(PATH + ARRAY[ELEMENT]);
+	}
+	return RESULT;
+}
+
+module.exports = function(grunt) {
+
+	var project = {
+		init: function() {
+			this.title = TITLE;
+			this.index = INDEX_PAGE;
+			this.language = LANGUAGE;
+			this.dir = DEVELOPMENT_DIR + "/";
+			var TEMPLATES_DIR_COMPILED = this.dir + TEMPLATES_DIR + "/",
+					RESOURCES_DIR_COMPILED = this.dir + RESOURCES_DIR + "/";
+			this.templates = {
+				dir: TEMPLATES_DIR_COMPILED,
+				css: TEMPLATES_DIR_COMPILED + CSS_TEMPLATE,
+			};
+			this.res = {
+				dir: RESOURCES_DIR_COMPILED,
+				css: {
+					dir: RESOURCES_DIR_COMPILED + CSS_DIR + "/",
+					devDir: RESOURCES_DIR_COMPILED + CSS_DEV_DIR + "/",
+					sass: RESOURCES_DIR_COMPILED + SASS_DIR + "/",
+					filename: CSS_FILENAME,
+					print: CSS_PRINT
+				}
+			};
+			this.build = {
+				dir: BUILD_DIR + "/",
+			};
+			return this;
+		}
+	}.init();
+
+	require("load-grunt-tasks")(grunt);
+
+	grunt.initConfig({
+
+		htmlhint: {
+			options: {
+				"htmlhintrc": ".htmlhintrc"
+			},
+			htmlHint: {
+				cwd: project.build.dir,
+				src: ["*.html"],
+				expand: true
+			}
+		},
+		csslint: {
+			option: {
+				"csslintrc": ".csslintrc"
+			},
+			cssLint: {
+				cwd: project.res.css.devDir,
+				src: ["*.css", "!*-IE.css"],
+				expand: true
+			}
+		},
+		csscss: {
+			options: {
+				verbose: true
+			},
+			csscssTest: {
+				src: project.res.css.devDir + "*.css"
+			}
+		},
+		colorguard: {
+			files: {
+				src: project.res.css.devDir + "*.css"
+			}
+		},
+
+		analyzecss: {
+			options: {
+				outputMetrics: "error",
+				softFail: true,
+				thresholds: grunt.file.readJSON(".analyzecssrc")
+			},
+			ananlyzeCSS: {
+				sources: [project.res.css.dir + project.res.css.filename + ".min.css"]
+			}
+		},
+
+		sass: {
+			options: {
+				sourceMap: true,
+				precision: 5
+			},
+			generateCSS: {
+				cwd: project.res.css.sass,
+				src: ["**/*.scss", "**/*.sass", "!**/_*.scss", "!**/_*.sass"],
+				dest: project.res.css.devDir,
+				ext: ".css",
+				expand: true
+			}
+		},
+		autoprefixer: {
+			options: {
+				map: true,
+				browsers: ["> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1", "Explorer >= 7"],
+				cascade: false
+			},
+			prefixCSS: {
+				cwd: project.res.css.devDir,
+				src: ["**/*.css", "!**/*-IE.css"],
+				dest: project.res.css.devDir,
+				expand: true
+			}
+		},
+
+		concat: {
+			css: {
+				src: "<%= TASK.CSS_ARRAY %>",
+				dest: project.res.css.dir + project.res.css.filename + ".css"
+			},
+			print: {
+				src: project.res.css.devDir + project.res.css.print + ".css",
+				dest: project.res.css.dir + project.res.css.print + ".css"
+			}
+		},
+
+		"string-replace": {
+			commentsFirst: {
+				options: {
+					replacements: [{
+						pattern: /\/\* line \d*, .* \*\/(\r?\n|\r)*/g,
+						replacement: ""
+					},{
+						pattern: /\/\*# sourceMappingURL(.|\t|\s|\r?\n|\r)*?\*\//gi,
+						replacement: ""
+					},{
+						pattern: /.media \-sass\-debug\-info(.|\t|\s|\r?\n|\r)*?\}\}/gi,
+						replacement: ""
+					},{
+						pattern: /\*\s(.)*\*\/(\r?\n|\r)*$/g,
+						replacement: ""
+					},{
+						pattern: /\*\s(.)*\*\/(\r?\n|\r)*\//g,
+						replacement: ""
+					}]
+				},
+				files: {
+					"./": [project.res.css.dir + "*.css"]
+				}
+			},
+			commentsSecond: {
+				options: {
+					replacements: [{
+						pattern: /(\r?\n|\r)*\/$/g,
+						replacement: ""
+					},{
+						pattern: /\/\*(.)*(\r?\n|\r){4}/g,
+						replacement: ""
+					}]
+				},
+				files: {
+					"./": [project.res.css.dir + "*.css"]
+				}
+			},
+			build: {
+				options: {
+					replacements: [{
+						pattern: /@tx-title/gi,
+						replacement: project.title + " Project"
+					},{
+						pattern: /@tx-language/gi,
+						replacement: project.language
+					},{
+						pattern: /@tx-launch/gi,
+						replacement: project.index
+					},{
+						pattern: /.!-- @tx-css -->(.|\t|\s|\r?\n|\r)*?!-- \/@tx-css -->/gi,
+						replacement: {
+							checkIE: function() {
+								var CSS_FILES,
+										CSS_PATH = project.res.css.dir.replace(project.dir, "");
+								if (grunt.file.exists(project.res.css.dir + project.res.css.filename + "-IE.css")) {
+									CSS_FILES = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + CSS_PATH + project.res.css.filename + ".min.css\">\n\t\t<!--[if lte IE 7]> <link rel=\"stylesheet\" type=\"text/css\" href=\"" + CSS_PATH + project.res.css.filename + "-IE.min.css\"> <![endif]-->";
+								} else {
+									CSS_FILES = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + CSS_PATH + project.res.css.filename + ".min.css\">";
+								}
+								return CSS_FILES;
+							}
+						}.checkIE()
+					}]
+				},
+				files: {
+					"./": [project.build.dir + "*.html"]
+				}
+			},
+			md: {
+				options: {
+					replacements:[{
+						pattern: /<\/dt><dd>/gi,
+						replacement: " "
+					},{
+						pattern: /<!DOCTYPE html>(.|\r?\n|\r)*<div class="headerContent">(\r?\n|\r)*/gi,
+						replacement: ""
+					},{
+						pattern: /<footer(.|\r?\n|\r)*<\/html>/gi,
+						replacement: ""
+					},{
+						pattern: /<span class="emplymentDate">/gi,
+						replacement: "– "
+					},{
+						pattern: /<\/span>(.|\r?\n|\r)*<span class="resumeNote">/gi,
+						replacement: " "
+					},{
+						pattern: /(\t)*<((?!>).)*>(\r?\n|\r)+/gi,
+						replacement: "\n\n"
+					},{
+						pattern: /(\t)*<((?!>).)*>/gi,
+						replacement: ""
+					},{
+						pattern: /(\n){3,}/gi,
+						replacement: "\n\n"
+					},{
+						pattern: /(\n)*(\t)+$/gi,
+						replacement: ""
+					}]
+				},
+				files: {
+					"./": [project.build.dir + "*.md"]
+				}
+			}
+		},
+
+		uncss: {
+			cssOptimize: {
+				options: {
+					ignore: [/.*-is-.*/, /.*-has-.*/, /.*-are-.*/, /js-.*/],
+					stylesheets: [project.res.css.dir.replace(project.dir, "") + project.res.css.filename + ".css"],
+					timeout: 1000
+				},
+				files: {
+					cssMinFiles: function() {
+						var cssMinFilesObject = {};
+						cssMinFilesObject[project.res.css.dir + project.res.css.filename + ".css"] = [project.dir + "*.html", "!" + project.dir + "*-emailTemplate.html"];
+						return cssMinFilesObject;
+					}
+				}.cssMinFiles()
+			}
+		},
+		csscomb: {
+			options: {
+				config: ".csscombrc"
+			},
+			cssSortBuild: {
+				cwd: project.res.css.dir,
+				src: ["*.css"],
+				dest: project.res.css.dir,
+				expand: true
+			},
+			cssSortDev: {
+				cwd: project.res.css.devDir,
+				src: ["*.css"],
+				dest: project.res.css.devDir,
+				expand: true
+			}
+		},
+		cssc: {
+			options: grunt.file.readJSON(".csscrc"),
+			cssOptimize: {
+				cwd: project.res.css.dir,
+				src: ["*.css"],
+				dest: project.res.css.dir,
+				ext: ".min.css",
+				expand: true
+			}
+		},
+		cssmin: {
+			cssMin: {
+				cwd: project.res.css.dir,
+				src: ["*.min.css"],
+				dest: project.res.css.dir,
+				expand: true
+			}
+		},
+
+		processhtml: {
+			options: {
+				includeBase: project.templates.dir,
+				commentMarker: "@tx-process",
+				recursive: true
+			},
+			templates: {
+				cwd: project.templates.dir,
+				src: ["*.html", "!_*.html"],
+				dest: project.dir,
+				ext: ".html",
+				expand: true
+			}
+		},
+		htmlmin: {
+			options: grunt.file.readJSON(".htmlminrc"),
+			cleanup: {
+				cwd: project.build.dir,
+				src: ["*.html"],
+				dest: project.build.dir,
+				expand: true
+			}
+		},
+
+		clean: {
+			res: [project.res.css.dir],
+			build: [project.build.dir],
+			buildRes: [project.build.dir + project.res.dir.replace(project.dir, "")]
+		},
+		copy: {
+			build: {
+				cwd: project.dir,
+				src: ["**/*.*", "!**/tx-*.*", "!**/templates/**", "!**/**-dev/**", "!**/tx/**"],
+				dest: project.build.dir,
+				expand: true
+			}
+		},
+
+		wkhtmltopdf: {
+			resumePDF: {
+				src: [project.build.dir + "*.html", "!" + project.build.dir + "*-emailTemplate.html", "!" + project.build.dir + "*.mail.html"],
+				dest: project.build.dir,
+			}
+		},
+		html2md: {
+			main: {
+				options: {
+					encoding: "utf8"
+				},
+				cwd: project.build.dir,
+				src: ["*.html", "!*-emailTemplate.html", "!*.mail.html"],
+				dest: project.build.dir,
+				expand: true
+			},
+		},
+
+		watch: {
+			options: {
+				spawn: false
+			},
+			htmlTemplates: {
+				files: [project.templates.dir + "*.html"],
+				tasks: ["processhtml"]
+			},
+			sass: {
+				files: [project.res.css.sass + "**/*.scss", project.res.css.sass + "**/*.sass"],
+				tasks: ["sass", "autoprefixer"]
+			},
+			livereloadWatch: {
+				options: {
+					livereload: true
+				},
+				files: [project.dir + "*.html", project.res.css.devDir + "**/*.css"]
+			}
+		},
+		concurrent: {
+			options: {
+				logConcurrentOutput: true,
+				limit: 3
+			},
+			projectWatch: ["watch:htmlTemplates", "watch:sass", "watch:livereloadWatch"]
+		},
+		wait: {
+			options: {
+				delay: "2500"
+			},
+			pdf: {}
+		}
+
+	});
+
+	grunt.registerTask("process-css", "CSS processing", function() {
+		var CSS_DIR_REGEX = new RegExp("<link(.)*href=\"" + project.res.css.devDir.replace(project.dir, ""), "g"),
+				CSS_ALL = grunt.file.read(project.templates.css)
+					.replace(/(.|\t|\s|\r?\n|\r)*?<!-- @tx-css -->/, "")
+					.replace(/<!-- \/@tx-css -->(.|\t|\s|\r?\n|\r)*/, "")
+					.replace(/^\t(.)*print(.)*/gm, "")
+					.replace(/\t/g, ""),
+				CSS = CSS_ALL
+					.replace(/<!--(.|\t|\s|\r?\n|\r)*/, "")
+					.replace(CSS_DIR_REGEX, "")
+					.replace(/\r?\n|\r/g, "")
+					.replace(/">$/, ""),
+				CSS_ARRAY = CSS.split("\">"),
+				CSS_EXPECTED = CSS_ARRAY.length,
+				CSS_ACTUAL = grunt.file.expand([project.res.css.devDir + "*.css", "!" + project.res.css.devDir + project.res.css.print + ".css", "!" + project.res.css.devDir + project.res.css.letter + ".css"]).length;
+		if ((CSS_EXPECTED === CSS_ACTUAL || (CSS_ARRAY[0] === "" && CSS_ACTUAL === 0))) {
+			if (CSS_ACTUAL === 0) {
+				grunt.log.writeln("No .css-files to process.");
+			} else {
+				var PROCESS_TASKS = [];
+				PROCESS_TASKS.push("concat:css");
+				PROCESS_TASKS.push("concat:print");
+				grunt.config.set("TASK.CSS_ARRAY", fillAnArray(CSS_ARRAY, project.res.css.devDir));
+				PROCESS_TASKS = PROCESS_TASKS.concat(["uncss", "string-replace:commentsFirst", "string-replace:commentsSecond", "csscomb", "cssc", "cssmin:cssMin"]);
+				grunt.task.run(PROCESS_TASKS);
+			}
+		} else {
+			var ERROR_MESSAGE = "";
+			if (CSS_EXPECTED > CSS_ACTUAL) {
+				ERROR_MESSAGE += "There's got to be more .css-files. ";
+			} else if (CSS_EXPECTED < CSS_ACTUAL) {
+				ERROR_MESSAGE += "Not all of the .css-files has been referenced. ";
+			}
+			grunt.fail.warn(ERROR_MESSAGE);
+		}
+	});
+
+	grunt.registerTask("cssInline", "Injecting CSS", function() {
+		var CSS_REGEX = new RegExp("<(.)*" + project.res.css.filename + ".min.css(.)*>", "g"),
+				CSS = "<style type=\"text/css\">" + grunt.file.read(project.res.css.dir + project.res.css.filename + ".min.css") + grunt.file.read(project.res.css.dir + project.res.css.print + ".min.css") + "</style>",
+				FILES = grunt.file.expand([project.build.dir + "*.html"]),
+				FILES_LENGTH = FILES.length,
+				FILE_INDEX = 0;
+		for (FILE_INDEX; FILE_INDEX < FILES_LENGTH; FILE_INDEX++) {
+			var PAGE = grunt.file.read(FILES[FILE_INDEX]).replace(CSS_REGEX, CSS);
+			grunt.file.write(FILES[FILE_INDEX], PAGE);
+		}
+	});
+
+	grunt.registerTask("quality", ["htmlhint", "csslint", "csscss", "colorguard"]);
+
+	grunt.registerTask("performance", ["analyzecss"]);
+
+	grunt.registerTask("generate-css", ["sass", "autoprefixer"]);
+
+	grunt.registerTask("watch-project", ["concurrent"]);
+
+	grunt.registerTask("compile", ["clean:res", "processhtml", "generate-css", "process-css"]);
+
+	grunt.registerTask("build", ["compile", "clean:build", "copy:build", "string-replace:build", "htmlmin:cleanup", "wkhtmltopdf", "wait", "html2md", "string-replace:md", "cssInline", "clean:buildRes"]);
+
+};
